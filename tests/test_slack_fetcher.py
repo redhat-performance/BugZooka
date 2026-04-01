246a86b6fdcfa80cb4f2c272aa99498ee159a8e5
ArthurChenCoding <artchen@redhat.com>
2026-04-01 14:42:47 -0400

    clean up
    Signed-off-by: ArthurChenCoding <artchen@redhat.com>

--- tests/test_slack_fetcher.py
+++ tests/test_slack_fetcher.py
@@ -51,22 +51,6 @@ def run_slack_fetcher_test(
     mock_post_message = mock_slack_post_message(posted_messages)
     mock_file_upload = mock_slack_file_upload(posted_messages)
     mock_conversations_history = mock_slack_conversations_history(test_messages)
-    mock_analysis = patch(
-        "bugzooka.integrations.slack_fetcher.download_and_analyze_logs",
-        return_value=MOCK_PROW_ANALYSIS,
-    )
-    mock_job_history_check = patch(
-        "bugzooka.integrations.slack_fetcher.check_url_ok",
-        return_value=(True, 200),
-    )
-    mock_job_history_stats = patch(
-        "bugzooka.integrations.slack_fetcher.fetch_job_history_stats",
-        return_value=(2, 10, 20, ":grey_exclamation:"),
-    )
-    mock_rag_disabled = patch.object(
-        SlackMessageFetcher, "_is_rag_enabled", return_value=False
-    )
-
     with patch("bugzooka.integrations.slack_client_base.WebClient") as mock_web_client:
         mock_client_instance = MagicMock()
         mock_client_instance.conversations_history = mock_conversations_history
@@ -110,21 +94,15 @@ def run_slack_fetcher_test(
                 return_value="Some recommended actions.",
             )
 
-        with mock_analysis:
-            with mock_job_history_check:
-                with mock_job_history_stats:
-                    with mock_rag_disabled:
-                        with filter_mock:
-                            with analysis_mock:
-                                # Create fetcher and run one fetch cycle
-                                logger = logging.getLogger("test")
-                                fetcher = SlackMessageFetcher(
-                                    channel_id=CHANNEL_ID, logger=logger
-                                )
+        with filter_mock:
+            with analysis_mock:
+                # Create fetcher and run one fetch cycle
+                logger = logging.getLogger("test")
+                fetcher = SlackMessageFetcher(channel_id=CHANNEL_ID, logger=logger)
 
-                                fetcher.fetch_messages(
-                                    enable_inference=enable_inference,
-                                )
+                fetcher.fetch_messages(
+                    enable_inference=enable_inference,
+                )
 
     return posted_messages
 
